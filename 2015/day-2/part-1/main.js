1d45bb18dccd66c0a5737a8e23d049152a46fe37
Mike Crawford <dev.random.87@gmail.com>
2022-12-18 13:08:27 -0800

    Solve

--- 2015/day-2/part-1/main.js
+++ 2015/day-2/part-1/main.js
@@ -10,14 +10,30 @@ const fs = require("fs");
 // Solution 1
 const calcWrappingPaper = (input) => {
   // Map input to array of packages
+  let result = 0;
   const inputArray = input
     .split("\n")
     .map((string) => string.split("x").map(Number));
 
-  const result = inputArray;
+  // Sort package dimensions
+  inputArray.forEach((element) => {
+    element.sort((a, b) => a - b).map(Number);
+    const length = element[0];
+    const width = element[1];
+    const height = element[2];
+    const smallestSideArea = element[0] * element[1];
+    // Calculate running total
+    result += Number(
+      2 * length * width +
+        2 * width * height +
+        2 * height * length +
+        smallestSideArea
+    );
+  });
+
   return result;
 };
 
 // Tests
-const userInput = "1x1x10\n2x3x4";
+const userInput = fs.readFileSync("./input.txt").toString();
 console.log(calcWrappingPaper(userInput));
