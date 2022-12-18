7c09783ea19c2691e1633880cf05c56f5280b44c
Mike Crawford <dev.random.87@gmail.com>
2022-12-18 13:16:46 -0800

    Remove tests

--- 2015/day-2/part-2/main.js
+++ 2015/day-2/part-2/main.js
@@ -1,10 +1,3 @@
-/*
-Tests
-
-    A present with dimensions 2x3x4 requires 2+2+3+3 = 10 feet of ribbon to wrap the present plus 2*3*4 = 24 feet of ribbon for the bow, for a total of 34 feet.
-    A present with dimensions 1x1x10 requires 1+1+1+1 = 4 feet of ribbon to wrap the present plus 1*1*10 = 10 feet of ribbon for the bow, for a total of 14 feet.
-*/
-
 const fs = require("fs");
 
 // Solution 2
