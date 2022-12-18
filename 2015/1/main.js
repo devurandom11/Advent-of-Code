b9607c79b8904d966a3aea9a63a0378455976f12
Mike Crawford <dev.random.87@gmail.com>
2022-12-17 22:05:23 -0800

    Start day 1

---
+++ 2015/1/main.js
@@ -0,0 +1,7 @@
+/* Test Case
+(()) and ()() both result in floor 0.
+((( and (()(()( both result in floor 3.
+))((((( also results in floor 3.
+()) and ))( both result in floor -1 (the first basement level).
+))) and )())()) both result in floor -3.
+*/
