c5b780bc00e33a10aca0be22adeef8f12006e67e
Mike Crawford <dev.random.87@gmail.com>
2022-12-18 00:10:54 -0800

    Create skeleton for AoC 2015/day-1/

--- addParts.sh
+++ addParts.sh
@@ -13,14 +13,20 @@ do
         [ ! -d "part-1" ] && mkdir {part-1,part-2} || echo "Directory already exists..."
         [ ! -f "README.md" ] && echo "# Advent of Code --- Day $day" > README.md || echo "README already exists";
         cd part-1
+        echo $(pwd)
         [ ! -f "main.js" ] && touch {main.js,input.txt} || echo "Files already exist...";
         cd ../
+        echo $(pwd)
         cd part-2
+        echo $(pwd)
         [ ! -f "main.js" ] && touch {main.js,input.txt} || echo "Files already exist...";
         cd ../
-        git add -A &&
-        git commit -am "Create skeleton for AoC ${year}${day}";
+        echo $(pwd)
+        git add -A
+        git commit -am "Create skeleton for AoC ${year}${day}"
         cd ../
+        echo $(pwd)
+        sleep 10;
     done
     
     cd ../
