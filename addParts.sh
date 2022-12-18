9d5c8162e7ab3b7403492a070dfa227d1bff26a0
Mike Crawford <dev.random.87@gmail.com>
2022-12-18 00:39:33 -0800

    Create skeleton for AoC 2021/day-1/

--- addParts.sh
+++ addParts.sh
@@ -4,7 +4,7 @@ years=$(cd /mnt/c/Users/mikec/OneDrive/Git/Advent-of-Code/ && ls -1d */)
 
 for year in $years;
 do
-    if [[ $year == '2015/' || $year == '2016/' || $year == '2017/' || $year == '2018/' || $year == '2019/' ]]
+    if [[ $year == '2015/' || $year == '2016/' || $year == '2017/' || $year == '2018/' || $year == '2019/' || $year == '2020/' ]]
     then
         continue
     fi
@@ -17,23 +17,18 @@ do
         [ ! -d "part-1" ] && mkdir {part-1,part-2} || echo "Directory already exists..."
         [ ! -f "README.md" ] && echo "# Advent of Code --- Day $day" > README.md || echo "README already exists";
         cd part-1
-        echo $(pwd)
-        [ ! -f "main.*" ] && touch {main.rs,input.txt} || echo "Files already exist...";
+        [ ! -f "main.*" ] && touch {main.ts,input.txt} || echo "Files already exist...";
         cd ../
-        echo $(pwd)
         cd part-2
-        echo $(pwd)
-        [ ! -f "main.js" ] && touch {main.rs,input.txt} || echo "Files already exist...";
+        [ ! -f "main.js" ] && touch {main.ts,input.txt} || echo "Files already exist...";
         cd ../
-        echo $(pwd)
-        
         cd ../
-        echo $(pwd)
+        git add -A
+        git commit -am "Create skeleton for AoC ${year}${day}"
         sleep 1;
     done
-    git add -A
-    git commit -am "Create skeleton for AoC ${year}"
+    
     echo "Done with year $year"
-    sleep 10;
+    sleep 1;
     cd ../
 done
