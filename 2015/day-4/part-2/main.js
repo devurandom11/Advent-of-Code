11c91b17011042e0d5f4e57593c3ff372eb76d1f
Mike Crawford <dev.random.87@gmail.com>
2022-12-19 15:52:44 -0800

    Update

--- 2015/day-4/part-2/main.js
+++ 2015/day-4/part-2/main.js
@@ -1,26 +1,32 @@
-// Solution 1
 const fs = require("fs");
 const crypto = require("crypto");
 
+function md5(inputString) {
+  const hash = crypto.createHash("md5");
+  hash.update(inputString);
+  return hash.digest("hex");
+}
+
 const mineMeSomeCoins = (input) => {
+  console.time("main");
   let hashFound = false;
   let testString = input;
+  let toTest;
   let i = 0;
   while (hashFound === false) {
     // Test the hash
     for (i; true; i++) {
-      let toTest = crypto
-        .createHash("MD5")
-        .update(testString)
-        .digest("hex")
-        .slice(0, 6);
-      if (toTest === "000000") {
+      toTest = md5(testString).slice(0, 5);
+      if (toTest === "00000") {
         hashFound = true;
-        return --i;
+        console.timeEnd("main");
+        break;
       }
       testString = input + i;
+      i++;
     }
   }
+  return i;
 };
 
 const input = fs.readFileSync("./input.txt").toString();
