1f14ce244eba852c559d654dbf9f175b059ffbe1
Mike Crawford <dev.random.87@gmail.com>
2022-12-18 16:44:05 -0800

    Solve 1 using set

--- 2015/day-3/part-1/main.js
+++ 2015/day-3/part-1/main.js
@@ -1,16 +1,38 @@
 const fs = require("fs");
 
 const calcTotalHousesWithPresents = (input) => {
-  let result = userInput.split("");
+  // Set starting point and add to set
+  let xcoord = 0;
+  let ycoord = 0;
+  const visitedSet = new Set();
+  const inputArray = input.split("");
+  visitedSet.add(`${xcoord}, ${ycoord}`);
+
+  // Update set with each coordinate
+  for (const item of inputArray) {
+    switch (item) {
+      case "<":
+        xcoord -= 1;
+        break;
+
+      case ">":
+        xcoord += 1;
+        break;
+
+      case "^":
+        ycoord += 1;
+        break;
+
+      case "v":
+        ycoord -= 1;
+        break;
+    }
+    visitedSet.add(`${xcoord}, ${ycoord}`);
+  }
+
+  let result = visitedSet.size;
   return result;
 };
 
 const userInput = fs.readFileSync("./input.txt").toString();
 console.log(calcTotalHousesWithPresents(userInput));
-
-// For example:
-
-// > delivers presents to 2 houses: one at the starting location, and one to the east.
-// ^>v< delivers presents to 4 houses in a square, including twice to the house at his starting/ending location.
-// ^v^v^v^v^v delivers a bunch of presents to some very lucky children at only 2 houses.
-// To play, please identify yourself via one of these services:
