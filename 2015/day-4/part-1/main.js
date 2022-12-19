a50666e5203e577b4fc17a09991d67ed42536e0e
Mike Crawford <dev.random.87@gmail.com>
2022-12-19 14:46:46 -0800

    Attempt solution

---
+++ 2015/day-4/part-1/main.js
@@ -0,0 +1,30 @@
+// If your secret key is pqrstuv, the lowest number it combines with to make an MD5 hash starting with five zeroes is 1048970; that is, the MD5 hash of pqrstuv1048970 looks like 000006136ef
+
+// Solution 1
+const fs = require("fs");
+const crypto = require("crypto");
+
+const mineMeSomeCoins = (input) => {
+  let hashFound = false;
+  let testString = input;
+  let i = 1048960;
+  while (hashFound === false) {
+    // Test the hash
+    for (i; i > -1; i++) {
+      let toTest = crypto
+        .createHash("MD5")
+        .update(testString)
+        .digest("hex")
+        .slice(0, 5);
+      if (toTest === "00000") {
+        hashFound = true;
+        return i;
+      }
+      testString = input + i;
+    }
+  }
+};
+
+const input = "pqrstuv";
+// const input = fs.readFileSync("./input.txt").toString();
+console.log(mineMeSomeCoins(input));
