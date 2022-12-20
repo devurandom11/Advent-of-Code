07f0e229304d1252776f84db0ea79a0e7d76ce1d
Mike Crawford <dev.random.87@gmail.com>
2022-12-19 20:10:40 -0800

    Stumped

--- 2015/day-5/part-2/main.js
+++ 2015/day-5/part-2/main.js
@@ -2,35 +2,52 @@
 
 const fs = require("fs");
 
+function testStrings(str) {
+  const regex = /(.)\1(?!\1)(.)\2/;
+  return regex.test(str);
+}
+
+function testRepeat(str) {
+  const regex = /([a-zA-Z])[a-zA-Z](?=[a-zA-Z]\1)/;
+  return regex.test(str);
+}
 const findMeSomeStrings = (input) => {
   const inputArray = input.split("\n");
   let niceStrings = 0;
 
   for (const testString of inputArray) {
-    for (let i = 0; i < testString.length - 1; i++) {
-      if (testString[i] === testString[i + 1]) {
-      }
-      continue;
-    }
-    let duplicate = 0;
-    const duplicateRegex = /([a-z])\1/; // Regular expression that matches any character followed by itself
-    if (duplicateRegex.test(testString)) {
-      // Test the string for a match
-      duplicate += 1;
-    }
-    if (duplicate === 0) {
-      continue;
-    }
-    const vowelsRegex = /[aeiou]/g; // Regular expression that matches any vowel
-    let vowelCount = (testString.match(vowelsRegex) || []).length; // Count the number of vowels in the string
+    let pair = false;
+    let repeat = false;
+    pair = testStrings(testString);
+    repeat = testRepeat(testString);
 
-    if (vowelCount < 3) {
-      continue;
+    // // Check for non-overlapping pairs
+    // for (let i = 0; i < testString.length - 3; i++) {
+    //   if (
+    //     testString.substring(i, i + 2) === testString.substring(i + 2, i + 4)
+    //   ) {
+    //     pair = true;
+    //     break;
+    //   }
+    // }
+
+    // // Check for repeating characters with one letter between them
+    // for (let i = 0; i < testString.length - 2; i++) {
+    //   if (testString[i] === testString[i + 2]) {
+    //     repeat = true;
+    //     break;
+    //   }
+    // }
+
+    // If both tests pass, increment niceStrings
+    if (pair && repeat) {
+      niceStrings += 1;
     }
-    niceStrings += 1;
   }
+
   return niceStrings;
 };
 
+// const input = "qjhvhtzxzqqjkmpb\nxxyxx\nuurcxstgmygtbstg\nieodomkazucvgmuy\n";
 const input = fs.readFileSync("./input.txt").toString();
 console.log(findMeSomeStrings(input));
