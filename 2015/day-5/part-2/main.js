4f7f1d2a4d313a8581b4f6df21d08cab219389a6
Mike Crawford <dev.random.87@gmail.com>
2022-12-19 21:52:23 -0800

    Restart

--- 2015/day-5/part-2/main.js
+++ 2015/day-5/part-2/main.js
@@ -2,47 +2,23 @@
 
 const fs = require("fs");
 
-function testStrings(str) {
-  const regex = /(.)\1(?!\1)(.)\2/;
-  return regex.test(str);
-}
+const testPair = (str) => {
+  return false;
+};
 
-function testRepeat(str) {
-  const regex = /([a-zA-Z])[a-zA-Z](?=[a-zA-Z]\1)/;
-  return regex.test(str);
-}
-const findMeSomeStrings = (input) => {
-  const inputArray = input.split("\n");
+const testRepeat = (str) => {
+  return false;
+};
+
+const findMeSomeStrings = (str) => {
+  const inputArray = str.split("\n");
   let niceStrings = 0;
+  let pair = false;
+  let repeat = false;
 
-  for (const testString of inputArray) {
-    let pair = false;
-    let repeat = false;
-    pair = testStrings(testString);
-    repeat = testRepeat(testString);
-
-    // // Check for non-overlapping pairs
-    // for (let i = 0; i < testString.length - 3; i++) {
-    //   if (
-    //     testString.substring(i, i + 2) === testString.substring(i + 2, i + 4)
-    //   ) {
-    //     pair = true;
-    //     break;
-    //   }
-    // }
-
-    // // Check for repeating characters with one letter between them
-    // for (let i = 0; i < testString.length - 2; i++) {
-    //   if (testString[i] === testString[i + 2]) {
-    //     repeat = true;
-    //     break;
-    //   }
-    // }
-
-    // If both tests pass, increment niceStrings
-    if (pair && repeat) {
-      niceStrings += 1;
-    }
+  // If both tests pass, increment niceStrings
+  if (pair && repeat) {
+    niceStrings += 1;
   }
 
   return niceStrings;
