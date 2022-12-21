459d9181ac4c87a9c43b4cecfd7a410fdc004e5e
Mike Crawford <dev.random.87@gmail.com>
2022-12-20 23:09:26 -0800

    Hooray!

--- 2015/day-5/part-2/main.js
+++ 2015/day-5/part-2/main.js
@@ -2,10 +2,19 @@
 const fs = require("fs");
 
 const testPair = (str) => {
-  if (str !== "a") return true;
+  for (let i = 0; i < str.length - 2; i++) {
+    const search = `${str[i] + str[i + 1]}`;
+    let searchStr1 = str.slice(0, i);
+    let searchStr2 = str.slice(i + 2, str.length);
+    if (searchStr1.includes(search) || searchStr2.includes(search)) return true;
+  }
+  return false;
 };
 
 const testRepeat = (str) => {
+  for (let i = 0; i < str.length - 2; i++) {
+    if (str[i] === str[i + 2]) return true;
+  }
   return false;
 };
 
@@ -28,6 +37,6 @@ const findMeSomeStrings = (str) => {
   return niceStrings;
 };
 
-// const input = "qjhvhtzxzqqjkmpb\nxxyxx\nuurcxstgmygtbstg\nieodomkazucvgmuy\n";
+// const input = "qjhvhtzxzqqjkmpb\nxxyxx\nuurcxstgmygtbstg\nieodomkazuccvucgmuy\n";
 const input = fs.readFileSync("./input.txt").toString();
 console.log(findMeSomeStrings(input));
