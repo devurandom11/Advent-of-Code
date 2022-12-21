f8baa6fff42fc3b4e2bab858fa4c074c99a778f4
Mike Crawford <dev.random.87@gmail.com>
2022-12-21 07:14:52 -0800

    Build logic structure

--- 2015/day-6/part-1/main.js
+++ 2015/day-6/part-1/main.js
@@ -1,8 +1,8 @@
 // Solution 1
+const { dir } = require("console");
 const fs = require("fs");
 
-const lightEmUp = (input) => {
-  const inputArray = input.split("\n");
+const buildGrid = () => {
   const rownum = 1000;
   const colnum = 1000;
   const grid = [];
@@ -18,6 +18,29 @@ const lightEmUp = (input) => {
     grid.push(row);
   }
   return grid;
+};
+
+const getCoords = (arr) => {
+  return arr;
+};
+
+const getDirections = (arr) => {
+  return arr;
+};
+
+const buildNewGrid = (grid, coords, directions) => {
+  const newGrid = [];
+  return newGrid;
+};
+
+const lightEmUp = (input) => {
+  const inputArray = input.split("\n");
+  const grid = buildGrid();
+  const coordinates = getCoords(inputArray);
+  const directions = getDirections(inputArray);
+  const newGrid = buildNewGrid(grid, coordinates, directions);
+
+  return newGrid;
 };
 const input =
   "turn on 0,0 through 999,999\ntoggle 0,0 through 999,0\nturn off 499,499 through 500,500\n";
