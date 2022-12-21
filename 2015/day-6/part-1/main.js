982bf75490da4e35298999e3f451359660bc1d38
Mike Crawford <dev.random.87@gmail.com>
2022-12-21 14:53:39 -0800

    Finish coords function

--- 2015/day-6/part-1/main.js
+++ 2015/day-6/part-1/main.js
@@ -1,6 +1,8 @@
 // Solution 1
 const fs = require("fs");
+const { type } = require("os");
 
+// Build starting grid
 const buildGrid = () => {
   const rownum = 1000;
   const colnum = 1000;
@@ -9,8 +11,8 @@ const buildGrid = () => {
     const row = [];
     for (let j = 0; j < colnum; j++) {
       row.push({
-        r: i,
-        c: j,
+        x: j,
+        y: i,
         status: false,
       });
     }
@@ -19,30 +21,36 @@ const buildGrid = () => {
   return grid;
 };
 
-const getCoords = (arr) => {
-  return arr;
+const getCoords = (str) => {
+  const coords = {};
+  coords["x1"] = parseInt(
+    str.split("through")[0].split(",")[
+      str.split("through")[0].split(",").length - 1
+    ]
+  );
+  coords["y1"] = parseInt(str.split("through")[0].split(",")[1]);
+  coords["x2"] = parseInt(str.split("through")[1].split(",")[0]);
+  coords["y2"] = parseInt(str.split("through")[1].split(",")[1]);
+  return coords;
 };
 
-const getDirections = (arr) => {
-  return arr;
-};
+const getDirections = (str) => {};
 
-const buildNewGrid = (grid, coords, directions) => {
-  const newGrid = [];
-  return newGrid;
-};
+const updateLights = (grid, coords, directions) => {};
 
 const lightEmUp = (input) => {
   const inputArray = input.split("\n");
-  const grid = buildGrid();
-  const coordinates = getCoords(inputArray);
-  const directions = getDirections(inputArray);
-  const newGrid = buildNewGrid(grid, coordinates, directions);
-
+  let grid = buildGrid();
+  for (const line of inputArray) {
+    const coordinates = getCoords(line);
+    const directions = getDirections(line);
+    grid = updateLights(grid, coordinates, directions);
+  }
   return grid;
 };
 const input =
   "turn on 0,0 through 999,999\ntoggle 0,0 through 999,0\nturn off 499,499 through 500,500\n";
 // const input = fs.readFileSync("./input.txt").toString();
 
-console.log(lightEmUp(input));
+const testing = lightEmUp(input);
+console.dir(testing);
