17ca9feaeb0439032c3c9e9d1e880c0bf2327c94
Mike Crawford <dev.random.87@gmail.com>
2022-12-22 12:38:08 -0800

    Setup skeleton

--- 2015/day-7/part-1/main.js
+++ 2015/day-7/part-1/main.js
@@ -1,1 +1,31 @@
 const { parseInput } = require("../../utils/input-parser");
+
+// Parse instructions splitting in "->"
+const parseInstructions = (str) => {
+  const instructions = [];
+  const startArr = str.split("\n");
+  for (const item of startArr) {
+    let rightStr = item.split("->")[1];
+    let leftStr = item.split("->")[0];
+    instructions.push({ leftVal: leftStr, rightVal: rightStr });
+  }
+  return instructions;
+};
+
+// Process instructions one at a time
+const processInstructions = (arr) => {
+  const finalArr = [];
+  for (const item of arr) {
+    finalArr.push(item);
+  }
+  return finalArr;
+};
+
+const main = () => {
+  const inputStr = parseInput("./input.txt");
+  const instructions = parseInstructions(inputStr);
+  const result = processInstructions(instructions);
+  console.log(result);
+};
+
+main();
