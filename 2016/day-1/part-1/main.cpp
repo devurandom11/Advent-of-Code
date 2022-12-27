96d927adb83063539f700c489ae5761624b0353c
Mike Crawford <dev.random.87@gmail.com>
2022-12-27 13:28:32 -0800

    Strip new lines

--- 2016/day-1/part-1/main.cpp
+++ 2016/day-1/part-1/main.cpp
@@ -2,9 +2,6 @@
 
 int main()
 {
-    std::string file_reader = parse_input("./input.txt");
-    std::cout << "The contents of the file are as follows:\n"
-              << file_reader << std::endl;
 
     return 0;
 }
--- 2016/utils/parse-input.h
+++ 2016/utils/parse-input.h
@@ -16,6 +16,13 @@ std::string parse_input(const std::string &file_name)
     std::string content;
     std::getline(infile, content);
 
+    size_t nl_index = content.find_last_of("\n");
+
+    if (nl_index != std::string::npos)
+    {
+        content.erase(nl_index);
+    }
+
     infile.close();
 
     return content;
