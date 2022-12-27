0dc2c64abc715fa10820ad66e1862f33c2a60eb6
Mike Crawford <dev.random.87@gmail.com>
2022-12-27 15:49:51 -0800

    Parse array

--- 2016/day-1/part-1/main.cpp
+++ 2016/day-1/part-1/main.cpp
@@ -1,10 +1,34 @@
 #include "../../utils/parse-input.h"
+#include <vector>
+#include <utility>
 
 int main()
 {
-    std::string input = parse_input("./input.txt");
+    std::string inputStr = parse_input("./input.txt");
+    std::stringstream ss(inputStr);
+    std::string item;
+    // Prep vector of pairs for direction and distance [["R", 1], ["L", 3],[...],...]
+    std::vector<std::pair<std::string, int>> output;
+    char direction = 'N';
+    int distance, solution = -1;
 
-    std::cout << input << std::endl;
+    // Convert to array
+    while (std::getline(ss, item, ','))
+    {
+        std::stringstream direction_stream(item);
+        direction_stream >> direction;
+        direction_stream >> distance;
+        output.emplace_back(std::make_pair(std::string(1, direction), distance));
+    }
 
+    std::cout << "Orignial Input: " << inputStr << std::endl;
+    std::cout << "Array conversion: ";
+    for (const auto &row : output)
+    {
+        std::cout << row.first << " : " << row.second << std::endl;
+    }
+    std::cout << std::endl;
+    std::cout << "\n\nSolution: "
+              << solution;
     return 0;
 }
