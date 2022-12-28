332db2282fe59a198c74cbc80154f8ace0a9bf32
Mike Crawford <dev.random.87@gmail.com>
2022-12-27 18:07:04 -0800

    Use lookup table and reduce one-liner

--- 2015/day-1/part-1/main.js
+++ 2015/day-1/part-1/main.js
@@ -2,16 +2,14 @@ const { parseInput } = require("../../utils/input-parser");
 
 // Solution 1
 const elevator = (inputText) => {
-  const inputArray = inputText.split("");
-  let floor = 0;
-  for (const item of inputArray) {
-    if (item === "(") {
-      floor++;
-    } else floor--;
-  }
+  // Create a lookup table
+  const lookup = { "(": 1, ")": -1 };
 
-  return floor;
+  // Use Array.reduce() to reduce the input array to a single value
+  return inputText.split("").reduce((floor, char) => floor + lookup[char], 0);
 };
 
 const firstInput = parseInput("./input.txt");
+console.time("start");
 console.log(elevator(firstInput));
+console.timeEnd("start");
