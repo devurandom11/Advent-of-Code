6e92cb7474708f887e478e5cb40777cbf3867c95
Mike Crawford <dev.random.87@gmail.com>
2022-12-27 16:19:13 -0800

    Argggg

--- 2016/day-1/part-1/main.cpp
+++ 2016/day-1/part-1/main.cpp
@@ -1,6 +1,8 @@
 #include "../../utils/parse-input.h"
 #include <vector>
 #include <utility>
+#include <thread>
+#include <chrono>
 
 int main()
 {
@@ -8,9 +10,10 @@ int main()
     std::stringstream ss(inputStr);
     std::string item;
     // Prep vector of pairs for direction and distance [["R", 1], ["L", 3],[...],...]
-    std::vector<std::pair<std::string, int>> output;
+    std::vector<std::pair<char, int>> output;
     char direction = 'N';
     int distance, solution = -1;
+    int x, y = 0;
 
     // Convert to array
     while (std::getline(ss, item, ','))
@@ -18,15 +21,10 @@ int main()
         std::stringstream direction_stream(item);
         direction_stream >> direction;
         direction_stream >> distance;
-        output.emplace_back(std::make_pair(std::string(1, direction), distance));
+        output.emplace_back(std::make_pair(direction, distance));
     }
 
-    std::cout << "Orignial Input: " << inputStr << std::endl;
-    std::cout << "Array conversion: ";
-    for (const auto &row : output)
-    {
-        std::cout << row.first << " : " << row.second << std::endl;
-    }
+    // Process instructions
     std::cout << std::endl;
     std::cout << "\n\nSolution: "
               << solution;
