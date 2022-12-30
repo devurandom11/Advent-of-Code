5de1870609103d9e5d616c1ce9065ab118293d55
Mike Crawford <dev.random.87@gmail.com>
2022-12-30 15:39:30 -0800

    Update input

--- 2015/day-8/part-1/main.js
+++ 2015/day-8/part-1/main.js
@@ -1,6 +1,34 @@
-const { parseInput } = require("../../utils/input-parser");
+const { Timer, parseInput } = require("../../utils/utils");
+const timer = new Timer();
+const main = () => {
+  const inputStr = parseInput("./input.txt");
+  const inputArr = inputStr.split("\n");
 
-const inputStr = parseInput("./input.txt");
-const inputArr = inputStr.split("\n");
-console.dir(inputArr, { maxArrayLength: null });
-console.dir(this);
+  // Get string literal size
+  const strLiteral = getLiteralSize(inputArr);
+
+  // Get memory size
+  const memSize = getMemSize(inputArr);
+};
+
+const getLiteralSize = (arr) => {
+  let strLiteralSize = 0;
+  for (const string of arr) {
+    strLiteralSize += string.length;
+  }
+  return strLiteralSize;
+};
+
+const getMemSize = (arr) => {
+  let memSize = 0;
+  for (const string of arr) {
+    for (const char of string) {
+      if (char === '"') {
+      }
+    }
+  }
+};
+
+timer.start();
+console.log(main());
+timer.end();
