ae325ad9a854ced824a447d8d03d0a976698ef23
Mike Crawford <dev.random.87@gmail.com>
2022-12-30 17:39:11 -0800

    Try to split on part 2

--- QOL Scripts/getReadme.sh
+++ QOL Scripts/getReadme.sh
@@ -4,19 +4,7 @@ years=$(cd /mnt/c/Users/mikec/OneDrive/Git/Advent-of-Code/ && ls -1d */)
 baseurl='https://adventofcode.com'
 sessionid='Cookie: session=53616c7465645f5f35215e7e4886a54d3ef305beab28ae4aac4664bb460e5c351e71af3fe9eafa722e74ec8ed18abff6479c90cca4273957dfef3d8c693c7bd9'
 cd /mnt/c/Users/mikec/OneDrive/Git/Advent-of-Code/
-for year in $years
-do
-    cd $year
-    days=$(ls -1d */)
-    for day in $days;
-    do
-        cd $day
-        rm -rf README.md
-        cd ../
-    done
-    cd ../
-done
-sleep 10000;
+
 for year in $years;
 do
     cd $year
@@ -26,7 +14,7 @@ do
         dayint=$(echo $day | awk -F'-' '{print $2}' | awk -F'/' '{print $1}' )
         cd $day
         # sed and awk to strip html tags and wrap in backticks "```" to respect original formatting
-        curl $baseurl/${year}day/${dayint} -H "${sessionid}"  | sed 's/<\/*[^>]*>//g' | awk '/--- Day/{f=1} /You can also \[Shareon/{f=0} f {print}' | awk 'BEGIN{print "```"} {print} END{print "```"}' > README.md &&
+        curl $baseurl/${year}day/${dayint} -H "${sessionid}"  | sed 's/<\/*[^>]*>//g' | awk '/--- Day/{f=1} /--- Part Two ---/{f=0; print "\n\n"} /You can also \[Shareon/{f=0} f {print}' | awk 'BEGIN{print "```"} {print} END{print "```"}' > README.md &&
         echo "Readme curled from $baseurl/${year}day/${dayint} complete!"
         cd ../
         sleep .1;
