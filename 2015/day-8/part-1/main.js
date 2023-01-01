8afee1c6867c1b092422564d34b3ed8dd6436b0f
Mike Crawford <dev.random.87@gmail.com>
2023-01-01 14:41:34 -0800

    Move double escape check to top

--- 2015/day-8/part-1/main.js
+++ 2015/day-8/part-1/main.js
@@ -16,15 +16,13 @@ const getMemSize = (arr) => {
     console.log("String: ", str);
     for (let i = 0; i < str.length; i++) {
       // Check if escape character is found
-      if (str[i] === "\\") {
+      if (str[i] === "\\" && str[i + 1] === "\\") {
+        reducedStr += "\\";
+        i += 1;
+        continue;
+      } else if (str[i] === "\\") {
         // Check conditions to know how to process
         switch (str[i + 1]) {
-          case "\\":
-            // The next character is also an escape characer so we need to add it and skip a step in the loop to not process it again.
-            reducedStr += str[i + 1];
-            i += 1;
-            break;
-
           case "x":
             // Process hex and skip next 3 abcde\x27abcde
             const hex = `0x${str[i + 2]}${str[i + 3]}`;
@@ -49,7 +47,10 @@ const main = () => {
   const inputStr = parseInput("./testinput.txt");
   // Main input
   // const inputStr = parseInput("./input.txt");
-  const inputArr = inputStr.split("\n").map((str) => str.trimEnd());
+  const inputArr = inputStr
+    .trim()
+    .split("\n")
+    .map((str) => str.trim());
   const results = {};
   // Get string literal size
   const strLiteral = getLiteralSize(inputArr);
