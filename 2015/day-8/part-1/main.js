c1cb5ffcf8165c96cfded4afff35ddb322ad556b
Mike Crawford <dev.random.87@gmail.com>
2023-01-01 12:55:04 -0800

    Trim end

--- 2015/day-8/part-1/main.js
+++ 2015/day-8/part-1/main.js
@@ -4,18 +4,24 @@ const timer = new Timer();
 const getLiteralSize = (arr) => {
   let strLiteralSize = 0;
   for (const string of arr) {
-    strLiteralSize += string.trimEnd().length;
+    strLiteralSize += string.length;
   }
   return strLiteralSize;
 };
 
-const getMemSize = (arr) => {};
+const getMemSize = (arr) => {
+  arr.map((str) => {
+    for (let i = 0; i < str.length; i++) {
+      console.log(str[i]);
+    }
+  });
+};
 
 const main = () => {
   // Test input
   const inputStr = parseInput("./testinput.txt");
   // const inputStr = parseInput("./input.txt");
-  const inputArr = inputStr.split("\n");
+  const inputArr = inputStr.split("\n").map((str) => str.trimEnd());
   const results = {};
   // Get string literal size
   const strLiteral = getLiteralSize(inputArr);
