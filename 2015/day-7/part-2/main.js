5d0a04f5000ec42547057c3bf26004b920edfd24
Mike Crawford <dev.random.87@gmail.com>
2022-12-31 16:12:10 -0800

    Build wire class

--- 2015/day-7/part-2/main.js
+++ 2015/day-7/part-2/main.js
@@ -1,1 +1,34 @@
-const { parseInput } = require("../../utils/input-parser");
+const { pause, parseInput, Timer } = require("../../utils/utils");
+const timer = new Timer();
+
+// Define wire
+class wire {
+  constructor(name, source) {
+    this.name = name;
+    this.source = source;
+    this.destinations = [];
+  }
+
+  addDestination(destination) {
+    this.destinations.push(destination);
+  }
+
+  getValue(wiringDiagram) {
+    const source = this.source;
+    let sourceVal;
+  }
+}
+
+// Build the wiring diagram
+const buildDiagram = (arr) => {};
+
+// Main
+const main = () => {
+  let result = -1;
+  const inputArr = parseInput("./input.txt").trimEnd().split("\n");
+
+  return result;
+};
+
+const result = main();
+console.log(`\nThe result is: ${result}\n`);
