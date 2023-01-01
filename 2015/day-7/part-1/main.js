419fa25f601a2c94987820ad0ef9e4aa747a023e
Mike Crawford <dev.random.87@gmail.com>
2022-12-31 17:13:24 -0800

    Capitalize array to avoid keyword collision

--- 2015/day-7/part-1/main.js
+++ 2015/day-7/part-1/main.js
@@ -1,38 +1,16 @@
 const { parseInput, Timer } = require("../../utils/utils");
 const timer = new Timer();
 
-// Capitalize to avoid keyword collision; Extract name, value, and source(s); Return adjacency list.
-const mapInput = (arr) => {
-  const mappedArr = arr.map((str) => {
-    let capitalStr = "";
-    for (let i = 0; i < str.length; i++) {
-      capitalStr += str[i].toUpperCase();
-    }
-    return capitalStr.split(" ");
-  });
-
-  const mappedInput = {};
-  for (let i = 0; i < mappedArr.length; i++) {
-    // Create object with strings
-    mappedInput.push({
-      originalString: mappedArr[i].join(" "),
-      assignment: mappedArr[i][mappedArr[i].indexOf("->") + 1],
-      rightVal: mappedArr[i][mappedArr[i].indexOf("->") - 1],
-      leftVal: mappedArr[i][mappedArr[i].indexOf("->") - 3],
-      operator: mappedArr[i][mappedArr[i].indexOf("->") - 2],
-    });
-
-    // Convert to Numbers
-    if (!isNaN(mappedInput[i].rightVal)) {
-      mappedInput[i].rightVal = Number(mappedInput[i].rightVal);
-    }
-    if (!isNaN(mappedInput[i].leftVal)) {
-      mappedInput[i].leftVal = Number(mappedInput[i].leftVal);
-    }
-  }
-  // Return array of objects
-  return mappedInput;
+// Capitalize to avoid keyword collision. Return capitalized array.
+const allCaps = (arr) => {
+  const capsArr = arr.reduce((acc, line) => {
+    acc.push(line.toUpperCase());
+    return acc;
+  }, []);
+  return capsArr;
 };
+
+// Extract name, value, and source(s); Return adjacency list.
 
 // Recursively search for node given adjacency list; Return target node object.
 const findWire = (graph, node) => {
@@ -41,11 +19,12 @@ const findWire = (graph, node) => {
 
 const main = () => {
   const inputArr = parseInput("./input.txt").trimEnd().split("\n");
-  const adjacencyList = mapInput(inputArr);
-  const resultWire = findWire(adjacencyList, "A");
-  const result = resultWire.value();
+  const capitalizedArray = allCaps(inputArr);
+  const adjacencyList = buildGrid(capitalizedArray);
+  // const resultWire = findWire(adjacencyList, "A");
+  // const result = resultWire.value();
 
-  return result;
+  return adjacencyList;
 };
 
 // Main program
