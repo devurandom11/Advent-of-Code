5cf509903e1000efad9a2b4a150f93e156e7b8b2
Mike Crawford <dev.random.87@gmail.com>
2023-01-01 13:25:23 -0800

    Test theory of peeking with sliding window

--- 2015/day-8/part-1/main.js
+++ 2015/day-8/part-1/main.js
@@ -1,25 +1,55 @@
-const { Timer, pause, parseInput } = require("../../utils/utils");
+const { Timer, parseInput } = require("../../utils/utils");
 const timer = new Timer();
 
+// Return full string length
 const getLiteralSize = (arr) => {
-  let strLiteralSize = 0;
-  for (const string of arr) {
-    strLiteralSize += string.length;
-  }
-  return strLiteralSize;
+  return arr.reduce((length, str) => {
+    return (length += str.length);
+  }, 0);
 };
 
+// Parse for mem size
 const getMemSize = (arr) => {
+  const returnArr = [];
   arr.map((str) => {
+    let reducedStr = "";
     for (let i = 0; i < str.length; i++) {
-      console.log(str[i]);
+      console.log("original string: ", str);
+
+      switch (str[i]) {
+        case "\\":
+          console.log("escape sequence found", str[i]);
+          console.log("peeking...", str[i - 1], str[i], str[i + 1]);
+          break;
+        case "\\x":
+          console.log("escape sequence found", str[i]);
+          console.log("peeking...", str[i], str[i + 1], str[i + 2]);
+          break;
+        case "\\\\":
+          console.log("escape sequence found", str[i]);
+          console.lg(
+            "peeking...",
+            str[i - 2],
+            str[i - 1],
+            str[i],
+            str[i + 1],
+            str[i + 2]
+          );
+          break;
+        default:
+          console.log("no escape sequence found", str[i]);
+          break;
+      }
     }
+    returnArr.push(reducedStr);
   });
+  return returnArr;
 };
 
 const main = () => {
   // Test input
   const inputStr = parseInput("./testinput.txt");
+  // Main input
   // const inputStr = parseInput("./input.txt");
   const inputArr = inputStr.split("\n").map((str) => str.trimEnd());
   const results = {};
