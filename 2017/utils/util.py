374c950292f4b755d50bea5d0d4896e1936f2edc
Mike Crawford <dev.random.87@gmail.com>
2023-01-08 13:09:44 -0800

    Create utility functions

--- 2017/utils/util.py
+++ 2017/utils/util.py
@@ -1,3 +1,19 @@
-# create a function that adds two numbers and export it as a module
-def add(a, b):
-    return a + b
+import time as t
+
+
+def parse_input(inputStr: str) -> list:
+    return inputStr.strip().split("\n")
+
+
+class Timer:
+    def __init__(self, name: str) -> None:
+        self.name: str = name
+
+    def start(self) -> None:
+        self.start_time: float = t.time()
+
+    def end(self) -> None:
+        self.end_time: float = t.time()
+
+    def __str__(self) -> str:
+        return f"{self.name} Timer: {self.end_time - self.start_time:.2f} seconds\n{self.name} Timer: {(self.end_time - self.start_time)*1000:.0f} milliseconds\n"
