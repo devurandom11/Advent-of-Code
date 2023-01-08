4be13c721a72f6f52f9760407079315dba9418ac
Mike Crawford <dev.random.87@gmail.com>
2023-01-08 13:36:22 -0800

    Build main function

--- 2017/day-1/part-1/main.py
+++ 2017/day-1/part-1/main.py
@@ -1,3 +1,27 @@
-import util
+## Import the util module
+import sys
 
-print(util.add(1, 2))
+sys.path.insert(1, "../../utils")
+import util as ut
+
+# Define the main function
+def solve() -> int:
+    solution: int = None
+    # Get input as a list of ints
+    input_list: list = ut.parse_input("./input.txt")
+    for i in range(len(input_list)):
+        input_list[i] = int(input_list[i])
+
+    # TODO: apply algorithm to solve the problem returning an int.
+
+    # return the solution
+    return solution
+
+
+# Start the program
+if __name__ == "__main__":
+    timer: ut.Timer = ut.Timer("Main")
+    timer.start()
+    print(solve())
+    timer.end()
+    print(timer)
