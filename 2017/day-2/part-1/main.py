90eccf77f34724c522db2e9c1aa4122dbb15c1e6
Mike Crawford <dev.random.87@gmail.com>
2023-01-08 15:31:37 -0800

    Sort and sum

--- 2017/day-2/part-1/main.py
+++ 2017/day-2/part-1/main.py
@@ -8,12 +8,25 @@ def solve() -> int:
     with open("./input.txt") as f:
         lines: list[str] = f.readlines()
         f.close()
-    matrix: list[list[int]] = [[int(x) for x in line.split("\t")] for line in lines]
-    print(matrix)
+    # Parse input into matrix
+    matrix = []
+    for line in lines:
+        # Split the line on tabs and remove any extra spaces
+        digits = [x.strip() for x in line.split("\t")]
+        # Convert the digits to integers and append them to the matrix
+        matrix.append([int(x) for x in digits])
+
+    # Calculate checksum
+    checksum = 0
+    for row in matrix:
+        row.sort()
+        checksum += row[-1] - row[0]
+
+    return checksum
 
 
 timer: ut.Timer = ut.Timer("Main")
 timer.start()
-solve()
+print(solve())
 timer.end()
 print(timer)
