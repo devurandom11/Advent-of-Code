ce80fec8e378643049c6e6a1b22b19ed4fc5ad17
Mike Crawford <dev.random.87@gmail.com>
2023-01-08 14:53:09 -0800

    Refactor to remove redundant str/int  conversion and function calls

--- 2017/day-1/part-1/main.py
+++ 2017/day-1/part-1/main.py
@@ -1,35 +1,31 @@
-## Import the util module
 import sys
 
 sys.path.insert(0, "../../utils")
 import util as ut
 
-# Define the main function
+
 def solve() -> int:
-    solution: int = None
-    # Get input as a list of ints
-    input_list: list = ut.parse_input("./input.txt")
-    ints: str = str(input_list[0])
-    # TODO: apply algorithm to solve the problem returning an int.
-    solutionDict = searchInts(ints)
-    # add all values in dict
-    solution = sum(solutionDict.values())
-    return solution
+    with open("./input.txt") as f:
+        input_str = f.read().strip()
+
+    # Initialize count of each digit to 0
+    counts = [0] * 10
+
+    # Iterate over the digits in the input string
+    for i in range(len(input_str) - 1):
+        if input_str[i] == input_str[i + 1]:
+            counts[int(input_str[i])] += int(input_str[i])
+
+    # Check if first and last digits are equal
+    if input_str[0] == input_str[-1]:
+        counts[int(input_str[0])] += int(input_str[0])
+
+    return sum(counts)
 
 
-# Search Algorithm
-def searchInts(inputInts: int) -> dict:
-    solutionDict: dict = {0: 0, 1: 0, 2: 0, 3: 0, 4: 0, 5: 0, 6: 0, 7: 0, 8: 0, 9: 0}
-
-    for i in range(0, len(inputInts) - 1):
-        if inputInts[i] == inputInts[i + 1]:
-            solutionDict[int(inputInts[i])] += int(inputInts[i])
-    if inputInts[0] == inputInts[-1]:
-        solutionDict[int(inputInts[0])] += int(inputInts[0])
-    return solutionDict
-
-
-# Start the program
 if __name__ == "__main__":
     timer: ut.Timer = ut.Timer("Main")
+    timer.start()
     print(solve())
+    timer.end()
+    print(timer)
