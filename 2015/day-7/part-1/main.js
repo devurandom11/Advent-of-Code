6319910179c1f5c2266e43f525aeb4efc3b14efd
Mike Crawford <dev.random.87@gmail.com>
2023-01-10 20:31:41 -0800

    Map buildWires logic and build tests

--- 2015/day-7/part-1/main.js
+++ 2015/day-7/part-1/main.js
@@ -19,13 +19,34 @@ const RSHIFT = (a, b) => (a >> b) & 0xffff;
 const NOT = (a) => ~a & 0xffff;
 const ASSIGN = (a) => a;
 
-class Wire {
-  constructor(token) {
-    this.name = token.target;
-    this.inputs = token.value;
-    this.operation = token.instruction;
+const solve = (wire) => {
+  const wiringDiagram = {};
+  switch (wires[wire].length) {
+    case 1: // Assignment
+      if (wires[wire][0].match(/[0-9]/)) {
+        return ASSIGN(parseInt(wires[wire][0]));
+      } else return solve(wires[wire][0]);
+    case 2: // Not or Single operation
+      break;
+    case 3: // Binary operation
+      break;
+    default: // Error
+      console.error("Invalid input");
+      break;
   }
-}
+};
 
 const input = parseInput("./testinput.txt");
 const wires = buildWires(input);
+
+console.log(wires);
+
+// Tests
+console.log(solve("X"));
+console.log(solve("Y"));
+console.log(solve("D"));
+console.log(solve("E"));
+console.log(solve("F"));
+console.log(solve("G"));
+console.log(solve("H"));
+console.log(solve("I"));
