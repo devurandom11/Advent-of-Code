c35491f562ba0530f51987aeeef404f2eac64e8f
Mike Crawford <dev.random.87@gmail.com>
2023-01-10 20:03:53 -0800

    build wires before recurive calls

--- 2015/day-7/part-1/main.js
+++ 2015/day-7/part-1/main.js
@@ -1,22 +1,16 @@
 const { parseInput } = require("../../utils/utils");
 
-const input = parseInput("./testinput.txt").toUpperCase();
-const inputArr = input.trim().split("\n");
-
-const tokens = inputArr.map((line) => {
-  const parts = line.trim().split(" ");
-  const isAssignment = parts.length === 3;
-  const isUnary = parts.length === 4;
-  const isBinary = parts.length === 5;
-  return isAssignment
-    ? { value: parts[0], instruction: parts[1], target: parts[2] }
-    : isUnary
-    ? { value: parts[1], instruction: parts[0], target: parts[3] }
-    : isBinary
-    ? { value: [parts[0], parts[2]], instruction: parts[1], target: parts[4] }
-    : console.error(`Invalid instruction: ${line}`);
-});
-
+const buildWires = (input) => {
+  const wires = {};
+  input
+    .toUpperCase()
+    .split("\n")
+    .map((line) => line.split(" -> "))
+    .forEach((wire) => {
+      wires[wire[1]] = wire[0].split(" ");
+    });
+  return wires;
+};
 // 16-bit version of bitwise operations
 const AND = (a, b) => a & b & 0xffff;
 const OR = (a, b) => a | (b & 0xffff);
@@ -26,38 +20,12 @@ const NOT = (a) => ~a & 0xffff;
 const ASSIGN = (a) => a;
 
 class Wire {
-  constructor(name) {
-    this.name = name;
-    this.inputs = [];
-    this.operation = null;
-  }
-
-  addInput(input) {
-    this.inputs.push(input);
-  }
-
-  setOperation(operation) {
-    this.operation = operation;
-  }
-
-  getValue() {
-    switch (this.operation) {
-      case "AND":
-        return AND(this.inputs[0].getValue(), this.inputs[1].getValue());
-      case "OR":
-        return OR(this.inputs[0].getValue(), this.inputs[1].getValue());
-      case "LSHIFT":
-        return LSHIFT(this.inputs[0].getValue(), this.inputs[1].getValue());
-      case "RSHIFT":
-        return RSHIFT(this.inputs[0].getValue(), this.inputs[1].getValue());
-      case "NOT":
-        return NOT(this.inputs[0].getValue());
-      case "ASSIGN":
-        return ASSIGN(this.inputs[0].getValue());
-      default:
-        console.error(`Invalid operation: ${this.operation}`);
-    }
+  constructor(token) {
+    this.name = token.target;
+    this.inputs = token.value;
+    this.operation = token.instruction;
   }
 }
 
-console.log(tokens);
+const input = parseInput("./testinput.txt");
+const wires = buildWires(input);
