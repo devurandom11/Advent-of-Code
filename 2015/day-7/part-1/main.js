67624a7b9517683d0d083cf0e4a80e8b83411a1a
Mike Crawford <dev.random.87@gmail.com>
2023-01-15 12:58:14 -0800

    Upgrade to map data struct

--- 2015/day-7/part-1/main.js
+++ 2015/day-7/part-1/main.js
@@ -1,10 +1,8 @@
-const { getRandomValues } = require("crypto");
 const { Timer, parseInput } = require("../../utils/utils.js");
-const { validateHeaderName } = require("http");
 const timer = new Timer();
 
 // Get array[0] = instructions and array[1] = targetWire
-const inputArr = parseInput("./input.txt")
+const inputArr = parseInput("./testinput.txt")
   .trim()
   .toUpperCase()
   .split("\n")
@@ -14,19 +12,13 @@ const inputArr = parseInput("./input.txt")
     return obj;
   }, {});
 
-const wireMap = {};
-
+const wireMap = new Map();
 for (const key in inputArr) {
   let [leftVal, operator, rightVal] = inputArr[key];
   if (leftVal === "NOT") {
     leftVal = undefined;
     operator = "NOT";
     rightVal = inputArr[key][1];
-  } else if (!isNaN(leftVal)) {
-    leftVal = parseInt(leftVal);
   }
-  wireMap[key] = { leftVal, operator, rightVal };
+  wireMap.set(key, { leftVal, operator, rightVal });
 }
-
-// console.log(inputArr);
-console.log(wireMap);
