0d381d6d3c585adad5227950bfcc8272b3be7b32
Mike Crawford <dev.random.87@gmail.com>
2023-01-15 12:28:45 -0800

    This is the object I needed

--- 2015/day-7/part-1/main.js
+++ 2015/day-7/part-1/main.js
@@ -1,8 +1,32 @@
+const { getRandomValues } = require("crypto");
 const { Timer, parseInput } = require("../../utils/utils.js");
+const { validateHeaderName } = require("http");
 const timer = new Timer();
 
 // Get array[0] = instructions and array[1] = targetWire
-const inputArr = parseInput("./testinput.txt")
+const inputArr = parseInput("./input.txt")
   .trim()
+  .toUpperCase()
   .split("\n")
-  .map((line) => line.split("->").map((side) => side.trim()));
+  .map((line) => line.split("->").map((side) => side.trim()))
+  .reduce((obj, line) => {
+    obj[line[1]] = line[0].split(" ");
+    return obj;
+  }, {});
+
+const wireMap = {};
+
+for (const key in inputArr) {
+  let [leftVal, operator, rightVal] = inputArr[key];
+  if (leftVal === "NOT") {
+    leftVal = undefined;
+    operator = "NOT";
+    rightVal = inputArr[key][1];
+  } else if (!isNaN(leftVal)) {
+    leftVal = parseInt(leftVal);
+  }
+  wireMap[key] = { leftVal, operator, rightVal };
+}
+
+// console.log(inputArr);
+console.log(wireMap);
