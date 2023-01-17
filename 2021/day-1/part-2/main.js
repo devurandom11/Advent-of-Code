8983f073ab67147eb4b5a9f694e80cbbcd289094
Mike Crawford <dev.random.87@gmail.com>
2023-01-17 15:31:36 -0800

    formatting

--- 2021/day-1/part-2/main.js
+++ 2021/day-1/part-2/main.js
@@ -1,35 +1,36 @@
 import fs from "fs";
 var inputType = {
-    main: "input.txt",
-    test: "testinput.txt",
+  main: "input.txt",
+  test: "testinput.txt",
 };
 var inputArr = fs
-    .readFileSync(inputType.main, "utf-8")
-    .trim()
-    .split("\n")
-    .map(function (num) { return parseInt(num); })
-    .map(function (val, i, arr) { return val + arr[i + 1] + arr[i + 2]; });
+  .readFileSync(inputType.main, "utf-8")
+  .trim()
+  .split("\n")
+  .map(function (num) {
+    return parseInt(num);
+  })
+  .map(function (val, i, arr) {
+    return val + arr[i + 1] + arr[i + 2];
+  });
 var tracker = {
-    ascending: 0,
-    descending: 0,
+  ascending: 0,
+  descending: 0,
 };
 inputArr.reduce(function (tracker, curr, index) {
-    if (index === 0) {
-        console.log("We are starting at ".concat(curr));
-    }
-    else if (index === inputArr.length - 1 || index === inputArr.length - 2) {
-        console.log("We are ending at ".concat(inputArr[inputArr.length - 3]));
-    }
-    else {
-        if (curr > inputArr[index - 1]) {
-            tracker.ascending++;
-            console.log("We are ascending from ".concat(inputArr[index - 1], " to ").concat(curr));
-        }
-        else if (curr < inputArr[index - 1]) {
-            tracker.descending++;
-            console.log("We are descending from ".concat(inputArr[index - 1], " to ").concat(curr));
-        }
-    }
-    return tracker;
+  if (index === 0) {
+    console.log("We are starting at ".concat(curr));
+  } else if (curr > inputArr[index - 1]) {
+    tracker.ascending++;
+    console.log(
+      "We are ascending from ".concat(inputArr[index - 1], " to ").concat(curr)
+    );
+  } else if (curr < inputArr[index - 1]) {
+    tracker.descending++;
+    console.log(
+      "We are descending from ".concat(inputArr[index - 1], " to ").concat(curr)
+    );
+  }
+  return tracker;
 }, tracker);
 console.log(tracker);
