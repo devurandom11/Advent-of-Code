cde2f3e83a6ee580c39d3713c78ade67f03c6deb
Mike Crawford <dev.random.87@gmail.com>
2023-01-17 14:56:56 -0800

    Solve. Remember to parseInt

--- 2021/day-1/part-1/main.js
+++ 2021/day-1/part-1/main.js
@@ -1,1 +1,29 @@
-"use strict";
+import fs from "fs";
+var inputType = {
+    main: "input.txt",
+    test: "testinput.txt",
+};
+var inputArr = fs
+    .readFileSync(inputType.main, "utf-8")
+    .trim()
+    .split("\n")
+    .map(function (num) { return parseInt(num); });
+var tracker = {
+    ascending: 0,
+    descending: 0,
+};
+inputArr.reduce(function (tracker, curr, index) {
+    if (index === 0) {
+        console.log("We are starting at ".concat(curr));
+    }
+    else if (curr > inputArr[index - 1]) {
+        tracker.ascending++;
+        console.log("We are ascending from ".concat(inputArr[index - 1], " to ").concat(curr));
+    }
+    else if (curr < inputArr[index - 1]) {
+        tracker.descending++;
+        console.log("We are descending from ".concat(inputArr[index - 1], " to ").concat(curr));
+    }
+    return tracker;
+}, tracker);
+console.log(tracker);
