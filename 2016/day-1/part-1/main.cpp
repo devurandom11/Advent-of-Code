51fff6d848b72c5d884d3678be5ec8c439923512
Mike Crawford <dev.random.87@gmail.com>
2023-02-16 21:06:59 -0800

    Just messing aroudn

--- 2016/day-1/part-1/main.cpp
+++ 2016/day-1/part-1/main.cpp
@@ -1,83 +1,84 @@
+#include <boost/algorithm/string.hpp>
+#include <fstream>
 #include <iostream>
+#include <sstream>
 #include <string>
 #include <vector>
-#include <fstream>
-#include <sstream>
-#include <boost/algorithm/string.hpp>
 
 auto main() -> int
 {
-    std::ifstream file("input.txt");
-    std::string instruction = "";
-    std::vector<std::string> instructions = {};
-    std::string pause = "";
-    int x = 0;
-    int y = 0;
-    char heading = 'N';
-    int steps = 0;
-    std::string direction = "";
+  std::ifstream file("input.txt");
+  std::string instruction = "";
+  std::vector<std::string> instructions = {};
+  std::string pause = "";
+  int x = 0;
+  int y = 0;
+  char heading = 'N';
+  int steps = 0;
+  std::string direction = "";
 
-    while (std::getline(file, instruction, ','))
+  while (std::getline(file, instruction, ','))
+  {
+    boost::trim(instruction);
+    instructions.push_back(instruction);
+  }
+
+  for (auto instruction : instructions)
+  {
+    direction = instruction.substr(0, 1);
+    steps = std::stoi(instruction.substr(1, instruction.length()));
+
+    if (direction == "R")
     {
-        boost::trim(instruction);
-        instructions.push_back(instruction);
+      if (heading == 'N')
+      {
+        heading = 'E';
+        x += steps;
+      }
+      else if (heading == 'E')
+      {
+        heading = 'S';
+        y -= steps;
+      }
+      else if (heading == 'S')
+      {
+        heading = 'W';
+        x -= steps;
+      }
+      else if (heading == 'W')
+      {
+        heading = 'N';
+        y += steps;
+      }
     }
-
-    for (auto instruction : instructions)
+    else if (direction == "L")
     {
-        direction = instruction.substr(0, 1);
-        steps = std::stoi(instruction.substr(1, instruction.length()));
-
-        if (direction == "R")
-        {
-            if (heading == 'N')
-            {
-                heading = 'E';
-                x += steps;
-            }
-            else if (heading == 'E')
-            {
-                heading = 'S';
-                y -= steps;
-            }
-            else if (heading == 'S')
-            {
-                heading = 'W';
-                x -= steps;
-            }
-            else if (heading == 'W')
-            {
-                heading = 'N';
-                y += steps;
-            }
-        }
-        else if (direction == "L")
-        {
-            if (heading == 'N')
-            {
-                heading = 'W';
-                x -= steps;
-            }
-            else if (heading == 'W')
-            {
-                heading = 'S';
-                y -= steps;
-            }
-            else if (heading == 'S')
-            {
-                heading = 'E';
-                x += steps;
-            }
-            else if (heading == 'E')
-            {
-                heading = 'N';
-                y += steps;
-            }
-        }
+      if (heading == 'N')
+      {
+        heading = 'W';
+        x -= steps;
+      }
+      else if (heading == 'W')
+      {
+        heading = 'S';
+        y -= steps;
+      }
+      else if (heading == 'S')
+      {
+        heading = 'E';
+        x += steps;
+      }
+      else if (heading == 'E')
+      {
+        heading = 'N';
+        y += steps;
+      }
     }
+  }
 
-    std::cout << "Final position: " << x << ", " << y << std::endl;
-    std::cout << "Distance: " << abs(x) + abs(y) << std::endl;
-    std::cin >> pause;
-    return 0;
+  std::cout << "Final position: " << x << ", " << y << std::endl;
+  std::cout << "Distance: " << abs(x) + abs(y) << std::endl;
+  std::cin >> pause;
+
+  return 0;
 }
--- 2016/day-5/part-1/main.cpp
+++ 2016/day-5/part-1/main.cpp
@@ -1,82 +1,71 @@
-#include <openssl/evp.h> // OpenSSL library for MD5 implementation
-#include <iostream>
-#include <string>
-#include <iomanip>
 #include <fstream>
+#include <iomanip>
+#include <iostream>
+#include <openssl/evp.h> // OpenSSL library for MD5 implementation
+#include <string>
 
 using namespace std;
 
-bool firstFiveZeros(string hex)
-{
-    for (auto i = 0; i < 5; i++)
-    {
-        if (hex[i] != '0')
-        {
-            return false;
-        }
+bool firstFiveZeros(string hex) {
+  for (auto i = 0; i < 5; i++) {
+    if (hex[i] != '0') {
+      return false;
     }
-    return true;
+  }
+  return true;
 }
 
-void makeHex(unsigned char *result, unsigned int result_len, string &hex)
-{
-    stringstream hexStream;
-    hexStream << hex << setfill('0');
+void makeHex(unsigned char *result, unsigned int result_len, string &hex) {
+  stringstream hexStream;
+  hexStream << hex << setfill('0');
 
-    for (auto i = 0; i < result_len; i++)
-    {
-        hexStream << setw(2) << static_cast<int>(result[i]);
-    }
-    hex = hexStream.str();
+  for (auto i = 0; i < result_len; i++) {
+    hexStream << setw(2) << static_cast<int>(result[i]);
+  }
+  hex = hexStream.str();
 }
 
-void printInLoop(string password, string hex)
-{
-    cout << "\r"
-         << "Cracking Hash... "
-         << "\033[1;32m"
-         << string(password)
-         << "\033[0m"
-         << hex.substr(password.length(), (8) - password.length())
-         << " | " << hex
-         << flush;
+void printInLoop(string password, string hex) {
+  cout << "\r"
+       << "Cracking Hash... "
+       << "\033[1;32m" << string(password) << "\033[0m"
+       << hex.substr(password.length(), (8) - password.length()) << " | " << hex
+       << flush;
 }
 
-auto main() -> int
-{
-    string input;
-    ifstream file("input.txt");
-    unsigned char result[EVP_MAX_MD_SIZE];
-    unsigned int result_len;
-    string password = "";
-    int append = 0;
-    string temp;
+auto main() -> int {
+  string input;
+  ifstream file("input.txt");
+  unsigned char result[EVP_MAX_MD_SIZE];
+  unsigned int result_len;
+  string password = "";
+  int append = 0;
+  string temp;
 
-    getline(file, input);
-    cout << "\033[2J\033[1;1H"; // Clear console
+  getline(file, input);
+  cout << "\033[2J\033[1;1H"; // Clear console
 
-    while (password.size() < 8)
-    {
-        temp = input + to_string(append);
+  while (password.size() < 8) {
+    temp = input + to_string(append);
 
-        EVP_MD_CTX *ctx = EVP_MD_CTX_new();
-        EVP_DigestInit_ex(ctx, EVP_md5(), NULL);
-        EVP_DigestUpdate(ctx, temp.c_str(), temp.size());
-        EVP_DigestFinal_ex(ctx, result, &result_len);
-        EVP_MD_CTX_free(ctx);
+    EVP_MD_CTX *ctx = EVP_MD_CTX_new();
+    EVP_DigestInit_ex(ctx, EVP_md5(), NULL);
+    EVP_DigestUpdate(ctx, temp.c_str(), temp.size());
+    EVP_DigestFinal_ex(ctx, result, &result_len);
+    EVP_MD_CTX_free(ctx);
 
-        string hex;
-        makeHex(result, result_len, hex);
+    string hex;
+    makeHex(result, result_len, hex);
 
-        if (firstFiveZeros(hex))
-            password += hex[5];
-        append++;
-        // printInLoop(password, hex);
-    }
+    if (firstFiveZeros(hex))
+      password += hex[5];
+    append++;
+    // printInLoop(password, hex);
+  }
 
-    cout << "\033[2J\033[1;1H" << endl; // Clear console
-    cout << "Password: " << password << endl;
-    cout << "Press ENTER to exit..." << endl;
-    cin.get();
-    return 0;
+  cout << "\033[2J\033[1;1H" << endl; // Clear console
+  cout << "Password: " << password << endl;
+  cout << "Press ENTER to exit..." << endl;
+  cin.get();
+  return 0;
 }
